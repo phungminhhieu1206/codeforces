a7bc2db5fe1a1b378ddc4ab714f386ea9bc0bdb4
Hieu Phung <phungminhhieu1206@gmail.com>
2024-08-20 12:49:40 +0700

    update

--- src/main/java/com/hieudev/sort/_3_QuickSort.java
+++ src/main/java/com/hieudev/sort/_3_QuickSort.java
@@ -8,13 +8,44 @@ public class _3_QuickSort {
         _3_QuickSort obj = new _3_QuickSort();
         int nums[] = {5, 1, 0, 0, 1, 2, 4};
 
-        obj.quickSort(nums);
+        obj.quickSort(nums, 0, nums.length-1);
         for (int i: nums) {
             System.out.print(i + ",");
         }
     }
 
-    public void quickSort(int[] nums) {
-        // solve...
+    public void quickSort(int[] nums, int l, int r) {
+        if (l<r) {
+            int pivot = findPivot(nums, l, r);
+            quickSort(nums, l, pivot-1);
+            quickSort(nums, pivot+1, r);
+        }
+    }
+
+    /**
+     * Chọn phần tử cuối làm pivot
+     * For từ đầu đến kế cuối
+     * flag: đánh dấu index của phần tử sẽ swap tiếp theo
+     */
+    public int findPivot(int[] arr, int l, int r) {
+        int pivot = arr[r];
+        int flag = l;
+
+        for(int j=l; j<r; j++) {
+            if(arr[j] < pivot) {
+
+                // swap
+                int temp = arr[flag];
+                arr[flag] = arr[j];
+                arr[j] = temp;
+
+                flag++;
+            }
+        }
+        int temp = arr[flag];
+        arr[flag] = arr[r];
+        arr[r] = temp;
+
+        return flag;
     }
 }
