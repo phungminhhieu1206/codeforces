75d0d5e39c9abf2f8dfe3a4b057a8fe133d63eeb
Hieu Phung <phungminhhieu1206@gmail.com>
2024-09-01 21:31:00 +0700

    update

--- src/main/java/com/hieudev/techtalk/graph/BFS/_1091_ShortestPathInBinaryMatrix/_1091_ShortestPathInBinaryMatrix.java
+++ src/main/java/com/hieudev/techtalk/graph/BFS/_1091_ShortestPathInBinaryMatrix/_1091_ShortestPathInBinaryMatrix.java
@@ -10,28 +10,30 @@ public class _1091_ShortestPathInBinaryMatrix
         System.out.println(shortestPathBinaryMatrix(new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}}));
     }
 
+    // Đường đi duyệt theo BFS là đường đi ngắn nhất rồi!
     public static int shortestPathBinaryMatrix(int[][] grid) {
-        int len = grid.length;
-        if (grid[0][0] == 1 || grid[len - 1][len - 1] == 1)
+        int n = grid.length;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
 
+        // Các hướng đi
         int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
-        boolean[][] visited = new boolean[len][len];
+        boolean[][] visited = new boolean[n][n];
         Deque<int[]> queue = new ArrayDeque<>();
 
         visited[0][0] = true;
-        queue.add(new int[]{0, 0, 1}); // r, c, distance
+        queue.add(new int[]{0, 0, 1}); // row, column, distance
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            if (cur[0] == len - 1 && cur[1] == len - 1) // reach dest
+            if (cur[0] == n - 1 && cur[1] == n - 1) // reach dest
                 return cur[2];
 
             for (int[] direction : directions) {
                 int r = cur[0] + direction[0];
                 int c = cur[1] + direction[1];
 
-                if (Math.min(r, c) < 0 || Math.max(r, c) >= len
+                if (Math.min(r, c) < 0 || Math.max(r, c) >= n
                         || grid[r][c] == 1 || visited[r][c])
                     continue;
 
